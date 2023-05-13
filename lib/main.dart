8c8c709f67fc9fe707a5006fa0b48143d8c9ed65
Vilvock <64989193+Vilvock@users.noreply.github.com>
2023-05-13 13:13:55 -0300

    ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+// @dart=2.9
 import 'package:flutter/material.dart';
 import 'package:flutter_agua_da_serra_app/ui/auth/login.dart';
 import 'package:flutter_agua_da_serra_app/ui/main/menu/products.dart';
